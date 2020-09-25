1877982491d1dfcbd00726efbb940ab40899a566
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-09-24 22:05:44 -0300

    Commit inicial

---
+++ valida_login.php
@@ -0,0 +1,28 @@
+<?php
+
+	//echo "Lógica ok!";
+	
+	/* ### Método GET ###
+
+	echo "<pre>";
+		print_r($_GET);
+	echo "</pre>";
+
+	echo "<br/>";
+
+	echo $_GET['email'];
+	echo "<br/>";
+	echo $_GET['senha'];
+	*/
+
+	### Método POST ###
+
+	echo "<pre>";
+		print_r($_POST);
+	echo "</pre>";
+
+	echo "<br/>";
+
+	echo $_POST['email'];
+	echo "<br/>";
+	echo $_POST['senha'];
