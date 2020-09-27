5ac2be54ac9dee8b1590d81aa10c1c3f0aa5e7e6
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2020-09-27 14:25:57 -0300

    Controle de perfil de usuário

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -73,6 +73,14 @@
 
                   $chamado_dados = explode('#', $chamado);
 
+                  // identificação do perfil do usuário
+                  if($_SESSION['perfil_id'] == 2){
+                    // apenas exibe o chamado criado pelo usuário logado
+                    if($_SESSION['id'] != $chamado_dados[0]){
+                      continue;
+                    }
+                  }
+
                   if(count($chamado_dados) < 3){
                     continue;
                   }
@@ -81,9 +89,9 @@
 
                   <div class="card mb-3 bg-light">
                     <div class="card-body">
-                      <h5 class="card-title"> <?= $chamado_dados[0] ?> </h5>
-                      <h6 class="card-subtitle mb-2 text-muted"> <?= $chamado_dados[1] ?> </h6>
-                      <p class="card-text"> <?= $chamado_dados[2] ?> </p>
+                      <h5 class="card-title"> <?= $chamado_dados[1] ?> </h5>
+                      <h6 class="card-subtitle mb-2 text-muted"> <?= $chamado_dados[2] ?> </h6>
+                      <p class="card-text"> <?= $chamado_dados[3] ?> </p>
 
                     </div>
                   </div>
--- valida_login.php
+++ valida_login.php
@@ -11,10 +11,19 @@
 	// variável para verificar se a autenticação teve sucesso
 	$usuario_autenticado = false;
 
+	// varável de ID do usuário
+	$usuario_id = null;
+
+	$usuario_perfil_id = null;
+
+	$perfis = array(1 => 'Administrativo', 2 => 'Usuario');
+
 	// Relação de usuários do sistema (sem BD)
 	$usuarios_app = array(
-		array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-		array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+		array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+		array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+		array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2)
 	);
 
 	/*
@@ -35,6 +44,8 @@
 
 		if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
 			$usuario_autenticado = true;
+			$usuario_id = $user['id'];
+			$usuario_perfil_id = $user['perfil_id'];
 		}
 
 	}
@@ -42,8 +53,9 @@
 	if($usuario_autenticado){
 		echo 'Usuário autenticado!';
 		$_SESSION['autenticado'] = 'SIM';
-		$_SESSION['x'] = 'Um valor';
-		$_SESSION['y'] = 'Outro valor';
+		$_SESSION['id'] = $usuario_id;
+		$_SESSION['perfil_id'] = $usuario_perfil_id;
+		// $_SESSION['y'] = 'Outro valor'; (criado para testes)
 		header('Location: home.php');
 	}else{
 		// echo 'Erro na autenticação do usuário!';
